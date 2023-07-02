6c976d97934d7ea7880dc31c737c45ac65cd217d
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-07-02 13:24:59 +0530

    final commit

--- Controllers/mailer.js
+++ Controllers/mailer.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 export async function MailSender(data){
     //delay setting up
     let num=data.delay<0 ? -1*data.delay : data.delay
-    let DELAY=num;
+    let DELAY=num>0?num*1000:100;
     //Creating transport
     let sender=nodemailer.createTransport({
         service:"gmail",
